c0f6653e4600ab1da1012fa21b0139d831cc11c6
Sean Trott <seantrott@icsi.berkeley.edu>
2015-11-23 16:56:37 -0800

    Run NLU updated.

---
+++ src/ecg/special_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Assumes PATH_TO is from ros_ecgworkbench/scripts
+export PATH_TO=$PWD/../src/ecg
+export ECG_FED=FED1
+python3 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
+
+export JYTHONPATH=$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
+jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
+#python3 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
+
