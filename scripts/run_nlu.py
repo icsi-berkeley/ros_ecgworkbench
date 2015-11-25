f1d459e8f12f12a9ed5ed521f9ec7637d8cf2e46
Sean Trott <seantrott@icsi.berkeley.edu>
2015-11-25 10:20:54 -0800

    py2 code

--- scripts/run_nlu.py
+++ scripts/run_nlu.py
@@ -6,8 +6,8 @@ import os
 from robots.robots_ui import RobotUserAgent
 if __name__=="__main__":
     path = os.getcwd() + "/../src/ecg/special_setup.sh"
-    rospy.loginfo("Starting NLU system...")
-    rospy.init_node("ecg_nlu")
+    #rospy.loginfo("Starting NLU system...")
+    #rospy.init_node("ecg_nlu")
 
     subprocess.Popen(["sh", path])
     rua = RobotUserAgent(["AgentUI"])
--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -2,9 +2,9 @@
 # Assumes PATH_TO is from ros_ecgworkbench/scripts
 export PATH_TO=$PWD/../src/ecg
 export ECG_FED=FED1
-python3 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
+python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/robot_solver.py ProblemSolver &
 
 export JYTHONPATH=$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
 jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
-#python3 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
+#python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
 
