5bcdbfa4198705c8fe45fb09b5e4f06b134949e4
Sean Trott <seantrott@icsi.berkeley.edu>
2015-11-30 15:27:25 -0800

    commit

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -2,9 +2,11 @@
 # Assumes PATH_TO is from ros_ecgworkbench/scripts
 export PATH_TO=$PWD/../src/ecg
 export ECG_FED=FED1
-python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
+python2.7 -i $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver
+#python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
 
-export JYTHONPATH=$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
+
+export JYTHONPATH=$JYTHONPATH:$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
 jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
 #python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
 
