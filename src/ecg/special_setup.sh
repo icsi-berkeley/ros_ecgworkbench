3f96640ac8bec02d3ec918ba93c03511b389480e
Sean Trott <seantrott@icsi.berkeley.edu>
2015-11-30 15:43:12 -0800

    run_nlu

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -2,7 +2,7 @@
 # Assumes PATH_TO is from ros_ecgworkbench/scripts
 export PATH_TO=$PWD/../src/ecg
 export ECG_FED=FED1
-python2.7 -i $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver
+python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
 #python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
 
 
