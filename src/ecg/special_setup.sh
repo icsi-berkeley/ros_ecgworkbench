ee64326c68ac55ba0839bb4ff6f8e3f933b5a665
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-01 09:29:37 -0800

    Fixed special_setup

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -9,5 +9,5 @@ python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py Proble
 
 export JYTHONPATH=$JYTHONPATH:$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
 jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
-python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
+#python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
 
