d08f237458ff7ddaf502af6194fd8769142f544f
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-01 09:28:43 -0800

    Fixed special_setup

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -7,8 +7,6 @@ export ECG_FED=FED1
 python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
 #python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
 
-# Hack
-alias jython=/home/nao/jython
 export JYTHONPATH=$JYTHONPATH:$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
 jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
 python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/robots_ui.py AgentUI 
