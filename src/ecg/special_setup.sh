31766227ca8d262df35e75c35c8fbd81189e02b4
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-02 09:36:48 -0800

    special setup

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -5,7 +5,7 @@
 export PATH_TO=$PWD/../src/ecg
 export ECG_FED=FED1
 #python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver
-python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/cci_solver.py ProblemSolver &
+python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver &
 
 export JYTHONPATH=$JYTHONPATH:$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
 jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
