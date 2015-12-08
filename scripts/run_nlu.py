4a273d8e2a63a6e3a79dfa5ca8ffd29dc6b83cfa
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-08 15:18:25 -0800

    fixed run_nlu?

--- scripts/run_nlu.py
+++ scripts/run_nlu.py
@@ -5,7 +5,8 @@ import os
 
 from robots.robots_ui import RobotUserAgent
 if __name__=="__main__":
-    path = os.getcwd() + "/../src/ecg/special_setup.sh"
+	dir_name = os.path.dirname(os.path.realpath(__file__))
+    path = dir_name + "/../src/ecg/special_setup.sh"
     #path = os.getcwd() + "/../src/ecg/special_setup.sh"
     rospy.loginfo("Starting NLU system...")
     rospy.init_node("ecg_nlu")
