dd44abfbbff0243ba5bec276ca367918322c7b8e
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-08 15:32:24 -0800

    fixed run_nlu?

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -2,11 +2,12 @@
 # Assumes PATH_TO is from ros_ecgworkbench/scripts
 
 
-export PATH_TO=$PWD/../src/ecg
+#export PATH_TO=$PWD/../src/ecg
+export PATH_TO="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 export ECG_FED=FED1
 #python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver
-#python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver &
-python2.7 ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver &
+python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver &
+#python2.7 ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver &
 
 #export JYTHONPATH=$JYTHONPATH:$PATH_TO/ecg_interface/framework_code/build/compling.core.jar:$PATH_TO/ecg_interface/framework_code/src/main/nluas/language
 #jython -m analyzer $PATH_TO/ecg_interface/ecg_grammars/compRobots.prefs &
