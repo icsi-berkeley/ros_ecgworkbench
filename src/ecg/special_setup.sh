55793a9455fac0fcbc114c917a4c58eaa58a0db0
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-08 16:34:16 -0800

    fixed run_nlu?

--- src/ecg/special_setup.sh
+++ src/ecg/special_setup.sh
@@ -3,7 +3,7 @@
 
 
 #export PATH_TO=$PWD/../src/ecg
-export PATH_TO="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+export PATH_TO="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 export ECG_FED=FED1
 export SOLVER=ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver
 #python2.7 $PATH_TO/ecg_interface/robot_code/src/main/robots/ros_solver.py ProblemSolver
