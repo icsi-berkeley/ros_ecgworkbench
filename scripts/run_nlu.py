67274d069c34f7b4a71d28e5dec91d76e9e6a045
Sean Trott <seantrott@icsi.berkeley.edu>
2015-12-11 10:32:03 -0800

    Ros

--- scripts/run_nlu.py
+++ scripts/run_nlu.py
@@ -5,14 +5,14 @@ import os
 
 from robots.robots_ui import RobotUserAgent
 if __name__=="__main__":
-    dir_name = os.path.dirname(os.path.realpath(__file__))
-    path = dir_name + "/../src/ecg/special_setup.sh"
-    #path = os.getcwd() + "/../src/ecg/special_setup.sh"
-    #rospy.loginfo("Starting NLU system...")
-    #rospy.init_node("ecg_nlu")
+    #dir_name = os.path.dirname(os.path.realpath(__file__))
+    #path = dir_name + "/../src/ecg/special_setup.sh"
+    path = os.getcwd() + "/../src/ecg/special_setup.sh"
+    rospy.loginfo("Starting NLU system...")
+    rospy.init_node("ecg_nlu")
 
     subprocess.Popen(["sh", path])
-    #rua = RobotUserAgent(["AgentUI"])
-    #rua.prompt()
+    rua = RobotUserAgent(["AgentUI"])
+    rua.prompt()
 
 
